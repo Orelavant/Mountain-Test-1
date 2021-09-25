29ad52c23c0aae48569428b0409c4ab644d53fb8
Orelavant <jvalero@haverford.edu>
2021-09-24 21:08:47 -0400

    more progress made

--- test.py
+++ test.py
@@ -8,11 +8,13 @@ import random as rand
 
 
 def main():
-    rows = 9
-    columns = 9
+    # Initializing
+    rows = 3
+    columns = 3
     mount_matrix = np.zeros((rows, columns))
 
-    # drop_ones(mount_matrix)
+    # Creating mountain
+    drop_ones(mount_matrix)
 
 
 def drop_ones(matrix):
@@ -30,43 +32,45 @@ def drop_ones(matrix):
     middle = math.floor(len(matrix[0]) / 2)
 
     # 1. Drop ones until a 1 is in the top row.
+    # curr_pos[0] is row position. curr_pos[1] is column position.
     while flag is True:
+        print(matrix)
         # Position to check for 1s
-        curr_row = 0
-        curr_col = middle
+        curr_pos = (0, middle)
 
         # Continue until either a 1 is hit or the ground is hit. If 1 is hit,
         # go to step 2. If ground is hit, stay in current position.
-        while matrix[curr_row][curr_col] != 1 or curr_row != ground:
-            curr_row += 1
+        while matrix[curr_pos] != 1 and curr_pos[0] != ground:
+            curr_pos = (curr_pos[0] + 1, curr_pos[1])
 
-        # Check stability if you haven't hit the ground
-        if curr_row != ground:
-            curr_row, curr_col = check_stability(curr_row, curr_col, ground)
-
-        # Mark place of ball
-        matrix[curr_row][curr_col] = 1
+        # If hit ground and position is 0, skip stability checks. Otherwise mark current position
+        if not(curr_pos[0] == ground and matrix[curr_pos] == 0):
+            check_stability(curr_pos, matrix, ground)
+        else:
+            matrix[curr_pos] = 1
 
         # Check if we've hit the top row.
-        if curr_row == 0:
+        if curr_pos[0] == 0:
             flag = False
 
 
-def check_stability(curr_row, curr_col, ground):
+def check_stability(curr_pos, matrix, ground):
     """
     :param curr_pos:
     :param ground:
     :return:
     """
     # Check the 3 positions for 1s
-    middle = curr_pos
-    left = (curr_pos[0], curr_pos[1] - 1)
-    right = (curr_pos[0], curr_pos[1] + 1)
+    # one_pos is the position the 1 might be placed
+    below = curr_pos
+    below_left = (curr_pos[0], curr_pos[1] - 1)
+    below_right = (curr_pos[0], curr_pos[1] + 1)
+    one_pos = (curr_pos[0] - 1, curr_pos[1])
     spun = False
     spin = -1
 
     # Continue falling until hit stable position or ground.
-    while middle != 1 and left != 1 and right != 1 or curr_pos[0] != ground:
+    while not(matrix[below] == 1 and matrix[below_left] == 1 and matrix[below_right] == 1) and one_pos[0] != ground:
         # Randomly decide whether to go left or right
         if not spun:
             spin = rand.randrange(0, 2)
@@ -74,17 +78,28 @@ def check_stability(curr_row, curr_col, ground):
 
         # If spin is 1, go right. If 0, go left.
         if spin == 1:
-            middle = (middle[0], middle[1] + 1)
+            below = (below[0], below[1] + 1)
         else:
-            middle = (middle[0], middle[1] - 1)
-        left = (middle[0], middle[1] - 1)
-        right = (middle[0], middle[1] + 1)
+            below = (below[0], below[1] - 1)
+        below_left = (below[0], below[1] - 1)
+        below_right = (below[0], below[1] + 1)
 
-    # If not on the ground, move the 1 on the stable formation.
-    if curr_pos[0] != ground:
-        middle = (middle[0] - 1, middle[1])
+        # If below is 0, one_pos falls
+        if matrix[below] == 0:
+            one_pos = below
 
-    return middle
+            # If not one ground, recalculate below, below_left, and below_right
+            if one_pos[0] != ground:
+                below = (one_pos[0] + 1, one_pos[1])
+                below_left = (one_pos[0], one_pos[1] - 1)
+                below_right = (one_pos[0], one_pos[1] + 1)
+
+        # TODO MAKE SURE BELOW AND OTHER CHECKS AREN'T OUT OF BOUNDS
+        if below:
+            pass
+
+    # Mark position once stable
+    matrix[one_pos] = 1
 
 
 
